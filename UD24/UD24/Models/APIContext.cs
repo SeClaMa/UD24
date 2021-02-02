60926743f2fdcd19df6c6e0181b9318c3c074994
SeClaMa <76478324+SeClaMa@users.noreply.github.com>
2021-02-02 13:20:44 +0100

    Ejercicio2

---
+++ UD24/UD24/Models/APIContext.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UD24.Models
+{
+    public class APIContext : DbContext
+    {
+        public APIContext(DbContextOptions<APIContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        public DbSet<Videos> Video { get; set; }
+
+
+        /* Para Definir Keys 
+         * protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cientifico>()
+                .HasKey(c => c.Dni);
+            modelBuilder.Entity<Asignado_a>()
+                .HasKey(c => new { c.Cientifico, c.Proyecto });
+        }*/
+        
+
+
+    }
+}
---
+++ UD24/UD24/Models/Cliente.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UD24.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Direccion { get; set; }
+        public int DNI { get; set; }
+        public DateTime Fecha { get; set; }
+
+
+    }
+}
