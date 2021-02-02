848af8c6aac8ed63f49d1f1822dac8a4ca5bfa38
SeClaMa <76478324+SeClaMa@users.noreply.github.com>
2021-02-02 12:36:35 +0100

    Ej1

---
+++ UD24/UD24/Models/APIContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace UD24.Models
+{
+    public class APIContext : DbContext
+    {
+        public APIContext(DbContextOptions<APIContext>options):base(options)
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+
+        
+
+
+    }
+}
